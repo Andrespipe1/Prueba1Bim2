7c47e584984e1fce26bf91cea1cf9ab771b43b20
POO <andresftma@gmail.com>
2024-07-12 12:39:49 -0500

    avance 100% :v

--- src/busqueda.java
+++ src/busqueda.java
@@ -3,7 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
 public class busqueda extends JFrame{
-    private JTextField nhis;
+    private JTextField ced;
     private JPanel panel1;
     private JButton buscar;
     private JLabel datos;
@@ -46,14 +46,14 @@ public class busqueda extends JFrame{
     }
 
     public void buscarDatos() throws SQLException {
-        int historial = Integer.parseInt(nhis.getText());
+        int cedula = Integer.parseInt(ced.getText());
         Connection connection = conexion();
-        String sql = "Select * from PACIENTE where n_historial_clinico=?;";
+        String sql = "Select * from PACIENTE where cedula=?;";
         PreparedStatement pstmt = connection.prepareStatement(sql);
-        pstmt.setInt(1, historial);
+        pstmt.setInt(1, cedula);
         ResultSet rs = pstmt.executeQuery();
         if (rs.next()) {
-            String cedula = rs.getString("cedula");
+            String cedul = rs.getString("cedula");
             String histori = rs.getString("n_historial_clinico");
             String nombre = rs.getString("nombre");
             String apellido = rs.getString("apellido");
@@ -61,7 +61,7 @@ public class busqueda extends JFrame{
             String edad = rs.getString("edad");
             String desc = rs.getString("descripcion_enfermedad");
 
-            datos.setText("Cedula: "+cedula+" N° historial Clinico"+histori+"\n Nombre: "+nombre+" "+apellido+" Telf: "+telefono+" Edad: "+edad+" Enfermedad: "+desc);
+            datos.setText("Cedula: "+cedul+" N° historial Clinico"+histori+"\n Nombre: "+nombre+" "+apellido+" Telf: "+telefono+" Edad: "+edad+" Enfermedad: "+desc);
             setSize(1000,500);
         } else {
             JOptionPane.showMessageDialog(null, "No se encontró un registro con ese código");
